7f3f81902a93a408b1416a20cfb058db982accdf
mengxianliang <mengxianliangok@163.com>
2017-09-25 10:45:09 +0800

    666

--- XLSlideMenuExample/XLSlideMenuExample/ViewController.m
+++ XLSlideMenuExample/XLSlideMenuExample/ViewController.m
@@ -23,20 +23,18 @@
     self.title = @"消息";
     
     //左
-    UIImageView *headerImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 37, 37)];
-    headerImageView.layer.cornerRadius = headerImageView.bounds.size.width/2.0f;
-    headerImageView.layer.masksToBounds = true;
-    headerImageView.image = [UIImage imageNamed:@"header"];
-    UITapGestureRecognizer *tap1 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(showLeft)];
-    [headerImageView addGestureRecognizer:tap1];
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:headerImageView];
+    UIButton *headerButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 37, 37)];
+    headerButton.layer.cornerRadius = headerButton.bounds.size.width/2.0f;
+    headerButton.layer.masksToBounds = true;
+    [headerButton setImage:[UIImage imageNamed:@"header"] forState:UIControlStateNormal];
+    [headerButton addTarget:self action:@selector(showLeft) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:headerButton];
     
     //右
-    UIImageView *moreImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 25, 25)];
-    moreImageView.image = [UIImage imageNamed:@"more"];
-    UITapGestureRecognizer *tap2 = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(showRight)];
-    [moreImageView addGestureRecognizer:tap2];
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:moreImageView];
+    UIButton *moreButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 25, 25)];
+    [moreButton setImage:[UIImage imageNamed:@"more"] forState:UIControlStateNormal];
+    [moreButton addTarget:self action:@selector(showRight) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:moreButton];
     
     
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height - 64)];
